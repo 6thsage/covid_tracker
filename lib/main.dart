78336e7403987a7e5a1a9fcfeb579d0427c7f5b0
Willard Shikami <willardshikami@gmail.com>
2020-01-10 11:47:02 +0300

    getting started

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Center(
+    child: Text(
+      "Hello World",
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
