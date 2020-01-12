3ea0762ecffa1c904433525bf2eaec94c64dca9d
Willard Shikami <willardshikami@gmail.com>
2020-01-12 14:54:36 +0300

    exploring & implementing buttons, Lists, snackbars

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
-import 'screens/home.dart';
+// import 'screens/home.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     title: "Flutter App",
-    home: Home(),
+    home: Scaffold(
+      body: listView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('FAB clicked');
+        },
+        child: Icon(Icons.add),
+      ),
+    ),
   ));
+}
+
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+    content: Text('You have clicked $item'),
+  );
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
+List<String> generateListItems() {
+  var items = List<String>.generate(100, (counter) => 'Item $counter');
+  return items;
+}
+
+Widget listView() {
+  var listItems = generateListItems();
+  var ourListView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      title: Text(listItems[index]),
+      onTap: () {
+        showSnackBar(context, listItems[index]);
+      },
+    );
+  });
+  return ourListView;
 }
