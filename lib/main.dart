d5249b41dfe0dcba3f21257d7d7c970ba2ca5d67
Willard Shikami <willardshikami@gmail.com>
2020-01-13 12:31:09 +0300

    Buiulding new app UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,57 +2,81 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: GetUserInput(),
+    title: 'Interest Calculator',
+    home: CalculatorForm(),
   ));
 }
 
-class GetUserInput extends StatefulWidget {
+class CalculatorForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _FlightDetails();
+    return _MainCalculcatorForm();
   }
 }
 
-class _FlightDetails extends State<GetUserInput> {
-  String nameInput = '';
-  var _countries = ['Kenya', 'Uganda', 'Tanzania'];
-  var currentlySelectedItem = 'Kenya';
+class _MainCalculcatorForm extends State<CalculatorForm> {
+  final minSpacing = 10.0;
+  var currencies = ['KES', 'USD', 'BTC', 'Other'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: (Text('App Title')),
+        title: Text('Home'),
       ),
       body: Container(
-        child: Column(
+        margin: EdgeInsets.all(minSpacing * 2),
+        child: ListView(
           children: <Widget>[
-            TextField(
-              
-              onSubmitted: (String userInput) {
-                setState(() {
-                  nameInput = userInput;
-                });
-              },
+            getImage(),
+            Padding(
+                padding: EdgeInsets.only(top: minSpacing, bottom: minSpacing),
+                child: TextField(
+                    decoration: InputDecoration(
+                        labelText: 'Principal',
+                        hintText: 'Please Enter Princial Amount',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        )))),
+            Padding(
+              padding: EdgeInsets.only(top: minSpacing, bottom: minSpacing),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Rate if Interest',
+                  hintText: 'Please Enter Interest Rate',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
             ),
-            DropdownButton<String>(
-              items: _countries.map((String singleCountryItem){
-                return DropdownMenuItem<String>(
-                  value: singleCountryItem,
-                  child: Text(singleCountryItem),
-                );
-              }).toList(),
-              onChanged: (String newValueSelected){
-                setState(() {
-                  currentlySelectedItem = newValueSelected;
-                });
-              },
-              value: currentlySelectedItem,
-            ),
-            Text('Your City is $nameInput')
+            Row(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Term',
+                    hintText: 'Enter your Term'
+                  )
+                  ,)
+              ],
+            )
           ],
         ),
       ),
     );
   }
+}
+
+Widget getImage() {
+  AssetImage assetImage = AssetImage('images/zom.png');
+  Image image = Image(
+    image: assetImage,
+    width: 100.0,
+    height: 100.0,
+  );
+
+  return Container(
+    child: image,
+  );
 }
